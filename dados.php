645afacfc3e94cee1e7f1bcf00069606dbdd1186
Merthol <guilhermer_l@hotmail.com>
2025-08-14 15:55:59 -0300

    Implementação das páginas iniciais e de livros, utilizando os recursos do php para navegar entre as páginas e buscar informações de uma base de dados criada.

---
+++ dados.php
@@ -0,0 +1,48 @@
+<?php
+$livros = [
+    [
+        'id' => 1,
+        'titulo' => 'O Senhor dos Anéis',
+        'autor' => 'J.R.R. Tolkien',
+        'descricao' => 'Uma épica jornada pela Terra Média.',
+        'avaliacoes' => 5,
+        'imagem' => 'https://example.com/imagem1.jpg',
+        'link' => '/livro.php?id=1'
+    ],
+    [
+        'id' => 2,
+        'titulo' => '1984',
+        'autor' => 'George Orwell',
+        'descricao' => 'Uma distopia sobre vigilância e totalitarismo.',
+        'avaliacoes' => 4,
+        'imagem' => 'https://example.com/imagem2.jpg',
+        'link' => '/livro.php?id=2'
+    ],
+    [
+        'id' => 3,
+        'titulo' => 'Dom Casmurro',
+        'autor' => 'Machado de Assis',
+        'descricao' => 'Um romance psicológico sobre ciúmes e traição.',
+        'avaliacoes' => 3,
+        'imagem' => 'https://example.com/imagem3.jpg',
+        'link' => '/livro.php?id=3'
+    ],
+    [
+        'id' => 4,
+        'titulo' => 'A Revolução dos Bichos',
+        'autor' => 'George Orwell',
+        'descricao' => 'Uma fábula política sobre a corrupção do poder.',
+        'avaliacoes' => 4,
+        'imagem' => 'https://example.com/imagem4.jpg',
+        'link' => '/livro.php?id=4'
+    ],
+    [
+        'id' => 5,
+        'titulo' => 'O Pequeno Príncipe',
+        'autor' => 'Antoine de Saint-Exupéry',
+        'descricao' => 'Uma história poética sobre amor e amizade.',
+        'avaliacoes' => 5,
+        'imagem' => 'https://example.com/imagem5.jpg',
+        'link' => '/livro.php?id=5'
+    ]
+];
---
+++ infos.php
@@ -0,0 +1,9 @@
+<?php
+require 'dados.php'; // Exige o arquivo dados.php para acessar os dados dos livros
+
+echo "<pre>";
+var_dump($_REQUEST);
+echo "</pre>";
+echo "<pre>";
+var_dump($_SERVER);
+echo "</pre>";
